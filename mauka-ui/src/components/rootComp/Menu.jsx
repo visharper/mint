67442f211948d0891495c05625e2429ea98555ca
vishal <pennies4v@gmail.com>
2023-10-22 11:24:32 -0700

    Fixed Menu

--- mauka-ui/src/components/rootComp/Menu.jsx
+++ mauka-ui/src/components/rootComp/Menu.jsx
@@ -1,15 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { Heading, ButtonGroup, Flex, Spacer, IconButton, useColorMode, useColorModeValue, Container, Button} from '@chakra-ui/react'
 import { SunIcon  } from '@chakra-ui/icons'
 
 function MenuBar() {
-    const {toggleColorMode} = useColorMode()
-    const bgColor = useColorModeValue("brand.default", "whiteAlpha.50")
-    const textColor = useColorModeValue("brand.default", "")
+    const [isLoggedIn, setIsLoggedIn] = useState(()=>false)
     return( 
         <ButtonGroup display={["none", "none", "flex", "flex"]} >
             <Button variant='ghost'>Sign Up</Button>
-            <Button variant='ghost'>Log in</Button>
+            {!isLoggedIn ? 
+                <Button variant='ghost'>Log in</Button>
+                : <Button variant='ghost'>Profile</Button>
+            }
          </ButtonGroup>
         // <Flex>
         //     <Flex
